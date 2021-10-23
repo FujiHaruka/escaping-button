98a93f5a9b4a9c71351ecfd76ad60821d4abf715
FujiHaruka <fizzyjohn1427@gmail.com>
2021-10-23 10:21:07 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef, useState, VFC } from "react";
+import "./App.css";
+
+type Viewport = {
+  width: number;
+  height: number;
+};
+
+type MousePosition = {
+  x: number;
+  y: number;
+};
+
+const useViewportSize = () => {
+  const [viewport, set] = useState<Viewport>({ width: 0, height: 0 });
+  const observeSize = useCallback((target: HTMLElement) => {
+    const observer = new ResizeObserver(([entry]) => {
+      if (!entry) {
+        return;
+      }
+      const rect = entry.target.getBoundingClientRect();
+      set({
+        width: rect.width,
+        height: rect.height,
+      });
+    });
+    observer.observe(target);
+  }, []);
+  return {
+    viewport,
+    observeSize,
+  };
+};
+
+const useMousePosition = () => {
+  const [mousePosition, set] = useState<MousePosition>({
+    x: 0,
+    y: 0,
+  });
+  const observeMousePosition = useCallback((target: HTMLElement) => {
+    target.addEventListener("mousemove", (ev: MouseEvent) => {
+      const { x, y } = ev;
+      set({ x, y });
+    });
+  }, []);
+  return {
+    mousePosition,
+    observeMousePosition,
+  };
+};
+
+const useEscapingTimer = () => {
+  const timer = setInterval(() => {}, 300);
+  return () => {
+    clearInterval(timer);
+  };
+};
+
+const EscapingButton: VFC<{
+  viewport: Viewport;
+  mousePosition: MousePosition;
+}> = ({ viewport, mousePosition }) => {
+  return <button>CLICK ME</button>;
+};
 
 function App() {
+  const backgroundRef = useRef<HTMLDivElement>(null);
+  const { viewport, observeSize } = useViewportSize();
+  const { mousePosition, observeMousePosition } = useMousePosition();
+
+  useEffect(() => {
+    const target = backgroundRef.current;
+    if (target) {
+      observeSize(target);
+      observeMousePosition(target);
+    }
+  }, [observeSize, observeMousePosition]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background" ref={backgroundRef}></div>;
+      <EscapingButton viewport={viewport} mousePosition={mousePosition} />
+    </>
   );
 }
 
